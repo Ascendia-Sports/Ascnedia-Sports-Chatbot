0dbc6f5f9a33dfedb732eeda57ed76e1c530ddb9
Ascendia-Sports <contact@ascendia-sports.com>
2025-04-29 22:50:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 @app.route("/whatsapp", methods=["POST"])
 @app.route("/whatsapp/", methods=["POST"])
 def whatsapp_webhook():
-
     return Response(
         '<?xml version="1.0" encoding="UTF-8"?><Response><Message>✅ Webhook reached!</Message></Response>',
         mimetype="application/xml"
     )
+
 @app.route("/", methods=["GET"])
 def root():
     return "Falco Online"
