3702adb8f025f977d3cccd51cdc7f2aaff54a6bf
Ascendia-Sports <contact@ascendia-sports.com>
2025-04-29 22:47:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
+@app.route("/whatsapp", methods=["POST"])
 @app.route("/whatsapp/", methods=["POST"])
 def whatsapp_webhook():
+
     return Response(
         '<?xml version="1.0" encoding="UTF-8"?><Response><Message>✅ Webhook reached!</Message></Response>',
         mimetype="application/xml"
